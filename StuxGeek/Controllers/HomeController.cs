f3870601b9d535e45d89636fc32e92f63a885e1c
Giani Noyez <giani.noyez@outlook.com>
2015-09-26 00:44:16 +0200

    Homepage now loaded from db

--- StuxGeek/Controllers/HomeController.cs
+++ StuxGeek/Controllers/HomeController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StuxGeek.Models;
 
 namespace StuxGeek.Controllers
 {
     public class HomeController : Controller
     {
+        StuxGeekContext db = new StuxGeekContext();
+
         public ActionResult Index()
         {
-            return View();
+            List<Post> listOfPosts = new List<Post>();
+            listOfPosts = db.Posts.ToList();
+            //listOfPosts = db.Posts.Take(10).ToList();
+            return View(listOfPosts);
         }
     }
 }
